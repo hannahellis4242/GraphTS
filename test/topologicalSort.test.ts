fe94df8993fc9e5f58eed9eed01f02066be6c737
Hannah Ellis <hannahellis4242@gmail.com>
2023-08-18 11:41:04 +0100

    Adding next test case

--- test/topologicalSort.test.ts
+++ test/topologicalSort.test.ts
@@ -1,5 +1,6 @@
 import Graph from "../src/Graph";
 import addVertex from "../src/addVertex";
+import addEdge from "../src/addEdge"
 import topologicalSort from "../src/topologicalSort";
 describe("topologicalSort", () => {
   test("empty graph", () => {
@@ -14,4 +15,14 @@ describe("topologicalSort", () => {
     expect(result).toHaveLength(1);
     expect(result).toContain(vertex);
   });
+  test("two vertices graph",()=>{
+    const graph = new Graph<null,string,null>(null,true);
+    const vertex1 = addVertex("A",graph);
+    const vertex2 = addVertex("B",graph);
+    addEdge(vertex1,vertex2,null,graph);
+    const result = topologicalSort(graph);
+    expect(result).toHaveLength(2);
+    expect(result[0]).toBe(vertex2);
+    expect(result[1]).toBe(vertex1)
+  })
 });
