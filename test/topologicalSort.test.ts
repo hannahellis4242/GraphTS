7aed5cb53ac3338b3f3a9303cbe6936ea627c04e
hannahellis4242 <hannahellis4242@gmail.com>
2023-08-18 18:22:58 +0100

    Adding enough basic cases to be satisfied

--- test/topologicalSort.test.ts
+++ test/topologicalSort.test.ts
@@ -2,6 +2,8 @@ import Graph from "../src/Graph";
 import addVertex from "../src/addVertex";
 import addEdge from "../src/addEdge";
 import topologicalSort from "../src/topologicalSort";
+import vertices from "../src/vertices";
+import edges from "../src/edges";
 describe("topologicalSort", () => {
   test("empty graph", () => {
     const graph = new Graph<null, string, null>(null, true);
@@ -24,5 +26,70 @@ describe("topologicalSort", () => {
     expect(result).toHaveLength(2);
     expect(result[0]).toBe(vertex1);
     expect(result[1]).toBe(vertex2);
+  });
+  test("three vertices in sequence", () => {
+    const graph = new Graph<null, string, null>(null, true);
+    const vertexA = addVertex("A", graph);
+    const vertexB = addVertex("B", graph);
+    const vertexC = addVertex("C", graph);
+    addEdge(vertexA, vertexB, null, graph);
+    addEdge(vertexB, vertexC, null, graph);
+    expect(vertices(graph)).toHaveLength(3);
+    expect(edges(graph)).toHaveLength(2);
+    const result = topologicalSort(graph);
+    expect(result).toHaveLength(3);
+    expect(result).toStrictEqual([vertexA, vertexB, vertexC]);
+  });
+  test("three vertices with linking child", () => {
+    const graph = new Graph<null, string, null>(null, true);
+    const vertexA = addVertex("A", graph);
+    const vertexB = addVertex("B", graph);
+    const vertexC = addVertex("C", graph);
+    addEdge(vertexB, vertexA, null, graph);
+    addEdge(vertexC, vertexA, null, graph);
+    expect(vertices(graph)).toHaveLength(3);
+    expect(edges(graph)).toHaveLength(2);
+    const result = topologicalSort(graph);
+    expect(result).toHaveLength(3);
+    expect(result).toStrictEqual([vertexB, vertexC, vertexA]);
+  });
+  test("three vertices with linking parent", () => {
+    const graph = new Graph<null, string, null>(null, true);
+    const vertexA = addVertex("A", graph);
+    const vertexB = addVertex("B", graph);
+    const vertexC = addVertex("C", graph);
+    addEdge(vertexA, vertexB, null, graph);
+    addEdge(vertexA, vertexC, null, graph);
+    expect(vertices(graph)).toHaveLength(3);
+    expect(edges(graph)).toHaveLength(2);
+    const result = topologicalSort(graph);
+    expect(result).toHaveLength(3);
+    expect(result).toStrictEqual([vertexA, vertexB, vertexC]);
+  });
+  test("three vertex dimond", () => {
+    const graph = new Graph<null, string, null>(null, true);
+    const vertexA = addVertex("A", graph);
+    const vertexB = addVertex("B", graph);
+    const vertexC = addVertex("C", graph);
+    const vertexD = addVertex("D", graph);
+    addEdge(vertexD, vertexB, null, graph);
+    addEdge(vertexD, vertexC, null, graph);
+    addEdge(vertexC, vertexA, null, graph);
+    addEdge(vertexB, vertexA, null, graph);
+    expect(vertices(graph)).toHaveLength(4);
+    expect(edges(graph)).toHaveLength(4);
+    const result = topologicalSort(graph);
+    expect(result).toHaveLength(4);
+    expect(result).toStrictEqual([vertexD, vertexB, vertexC, vertexA]);
+  });
+  test("cycle", () => {
+    const graph = new Graph<null, string, null>(null, true);
+    const vertexA = addVertex("A", graph);
+    const vertexB = addVertex("B", graph);
+    const vertexC = addVertex("C", graph);
+    addEdge(vertexA, vertexB, null, graph);
+    addEdge(vertexB, vertexC, null, graph);
+    addEdge(vertexC, vertexA, null, graph);
+    expect(() => topologicalSort(graph)).toThrow("Graph contains a cycle");
   });
 });
