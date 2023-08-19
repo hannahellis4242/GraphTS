3eea2318563f96e0e49b43e297a70a66e678b18f
hannahellis4242 <hannahellis4242@gmail.com>
2023-08-19 16:46:19 +0100

    adding index.ts

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import Graph from "./Graph";
+import addEdge from "./addEdge";
+import addVertex from "./addVertex";
+import adjacentVertices from "./adjacentVertices";
+import clone from "./clone";
+import EdgeDescriptor from "./EdgeDescriptor";
+import edges from "./edges";
+import inDegree from "./inDegree";
+import inEdges from "./inEdges";
+import outDegree from "./outDegree";
+import outEdges from "./outEdges";
+import removeVertex from "./removeVertex";
+import source from "./source";
+import target from "./target";
+import topologicalSort from "./topologicalSort";
+import VertexDescriptor from "./VertexDescriptor";
+import vertices from "./vertices";
+
+export default Graph;
+export {
+  addEdge,
+  addVertex,
+  adjacentVertices,
+  clone,
+  EdgeDescriptor,
+  edges,
+  inDegree,
+  inEdges,
+  outDegree,
+  outEdges,
+  removeVertex,
+  source,
+  target,
+  topologicalSort,
+  VertexDescriptor,
+  vertices,
+};
